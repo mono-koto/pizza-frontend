5ca0e41aa1e8941e183b1d6c3155838b9b528889
Brent Fitzgerald <brent@mono-koto.com>
2024-01-24 12:54:53 -0800

    fix apostrophy escaping

--- src/app/[splitterAddr]/not-found.tsx
+++ src/app/[splitterAddr]/not-found.tsx
@@ -5,7 +5,7 @@ export default async function NotFound() {
     <div className='flex h-full flex-1 flex-col items-center justify-center gap-3 opacity-40 md:min-h-[200px]'>
       <Cookie />
       <div className='flex flex-col gap-4'>
-        <div>Splitter not found. Maybe it's on another chain?</div>
+        <div>Splitter not found. Maybe it&apos;s on another chain?</div>
       </div>
     </div>
   );
